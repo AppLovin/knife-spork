dcf6c40aaae540a8de9617471308293e03530003
jose.asuncion@gmail.com <jose.asuncion@gmail.com>
2015-06-04 18:23:50 -0700

    create attribute if missing

--- lib/knife-spork/utils.rb
+++ lib/knife-spork/utils.rb
@@ -1,11 +1,15 @@
 module KnifeSpork
   module Utils
-    def self.hash_set_recursive(attr, to, hash) 
+    def self.hash_set_recursive(attr, to, hash, create_if_missing = false) 
       if ! attr.include? "."
         hash[attr] = to
       else 
         head, *tail = attr.split(".")
-        hash[head] = hash_set_recursive(tail.join("."), to, hash[head])    
+        if ! hash[head].nil? 
+          hash[head] = hash_set_recursive(tail.join("."), to, hash[head], create_if_missing = create_if_missing) 
+        elsif create_if_missing
+          hash[head] = hash_set_recursive(tail.join("."), to, {}, create_if_missing = true) 
+        end 
       end
 
       hash
--- spec/unit/spork_utils_spec.rb
+++ spec/unit/spork_utils_spec.rb
@@ -30,6 +30,33 @@ module KnifeSpork
         json = Utils.hash_set_recursive("object1.object2.attribute", 2, json)
         expect(json["object1"]["object2"]["attribute"]).to eq(2)
       end
+
+      it "changes value of nonexistent nested attribute" do 
+        json = {
+          "object1" => {
+            "object2" => {
+              "attribute" => 1
+            }
+          }
+        }
+
+        json = Utils.hash_set_recursive("object1.object3.attribute", 2, json, create_if_missing=true)
+        expect(json["object1"]["object3"]["attribute"]).to eq(2)
+      end
+
+      
+      it "does not change value of nonexistent nested attribute" do 
+        json = {
+          "object1" => {
+            "object2" => {
+              "attribute" => 1
+            }
+          }
+        }
+
+        json = Utils.hash_set_recursive("object1.object3.attribute", 2, json)
+        expect(json["object1"]["object3"].nil?).to eq(true)
+      end
     end
   end
 end
