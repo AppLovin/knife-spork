544dcb41d60c510bcf6c234051d6bf8d43d7778a
jose.asuncion@gmail.com <jose.asuncion@gmail.com>
2015-06-08 10:32:59 -0700

    refactor spork_utils_spec

--- spec/unit/spork_utils_spec.rb
+++ spec/unit/spork_utils_spec.rb
@@ -2,96 +2,50 @@ require 'knife-spork/utils'
 
 module KnifeSpork
   describe Utils do
-    describe "walk_and_replace" do
+    describe "hash_set_recursive" do
+      environment = {
+        "object1" => {
+          "object2" => {
+          "attribute" => 1
+          }
+        },
+        "object" => 2,
+        "object4" => "some_string value"
+      }
 
       it "changes value of first level attribute" do 
-        json = {
-          :object1 => {
-            :object2 => {
-              :attribute => 1
-            }
-          },
-          "object3" => 2
-        }
-
-        json = Utils.hash_set_recursive("object3", 1, json) 
-        expect(json["object3"]).to eq(1)
+        environment = Utils.hash_set_recursive("object3", 1, environment) 
+        expect(environment["object3"]).to eq(1)
       end
 
       it "changes value of nested attribute" do 
-        json = {
-          "object1" => {
-            "object2" => {
-              "attribute" => 1
-            }
-          }
-        }
-
-        json = Utils.hash_set_recursive("object1#object2#attribute", 2, json)
-        expect(json["object1"]["object2"]["attribute"]).to eq(2)
+        environment = Utils.hash_set_recursive("object1#object2#attribute", 2, environment)
+        expect(environment["object1"]["object2"]["attribute"]).to eq(2)
       end
 
       it "changes value of nonexistent nested attribute" do 
-        json = {
-          "object1" => {
-            "object2" => {
-              "attribute" => 1
-            }
-          }
-        }
-
-        json = Utils.hash_set_recursive("object1#object3#attribute", 2, json, create_if_missing=true)
-        expect(json["object1"]["object3"]["attribute"]).to eq(2)
+        environment = Utils.hash_set_recursive("object1#object3#attribute", 2, environment, create_if_missing=true)
+        expect(environment["object1"]["object3"]["attribute"]).to eq(2)
       end
 
-      it "changes string value of existing nested attribute" do 
-        json = {
-          "object1" => "object1_value"
-        }
-
-        json = Utils.hash_set_recursive("object1#object3#attribute", 2, json, create_if_missing=true)
-        expect(json["object1"]["object3"]["attribute"]).to eq(2)
+      it "changes string value of existing nested attribute when create_if_missing=true" do 
+        environment = Utils.hash_set_recursive("object4#object5#attribute", 2, environment, create_if_missing=true)
+        expect(environment["object4"]["object5"]["attribute"]).to eq(2)
       end
 
-      
-      it "does not change value of existing nested attribute" do 
-        json = {
-          "object1" => {
-            "object2" => {
-              "attribute" => 1
-            }
-          }
-        }
-
-        json = Utils.hash_set_recursive("object1#object3#attribute", 2, json)
-        expect(json["object1"]["object3"].nil?).to eq(true)
+      it "does not change value of non-existent nested attribute by default" do 
+        environment = Utils.hash_set_recursive("object4#object5#attribute", 2, environment)
+        expect(environment["object4"]["object6"].nil?).to eq(true)
       end
 
       it "changes value of attribute that is # delimited" do 
-        json = {
-          "object1" => {
-            "object2" => {
-              "attribute" => 1
-            }
-          }
-        }
-
-        json = Utils.hash_set_recursive("object1#object2#attribute.something", 2, json)
-        expect(json["object1"]["object2"]["attribute.something"]).to eq(2)
+        environment = Utils.hash_set_recursive("object1#object2#attribute.something", 2, environment)
+        expect(environment["object1"]["object2"]["attribute.something"]).to eq(2)
       end
 
-      
       it "changes value of first level attribute that is # delimited" do 
-        json = {
-          "object1" => {
-            "object2" => {
-              "attribute" => 1
-            }
-          }
-        }
-
-        json = Utils.hash_set_recursive("object1#attribute.something", 2, json)
-        expect(json["object1"]["attribute.something"]).to eq(2)
+        environment = Utils.hash_set_recursive("object1#attribute.something", 2, environment)
+        expect(environment["object1"]["attribute.something"]).to eq(2)
       end
     end
   end
