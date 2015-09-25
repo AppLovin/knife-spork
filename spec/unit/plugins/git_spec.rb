4f09569c465ea06072793aa5c5244b87d1c7eb4a
jeunito <jeunito@gmail.com>
2015-09-25 15:08:34 -0700

    use Tempfile instead

--- spec/unit/plugins/git_spec.rb
+++ spec/unit/plugins/git_spec.rb
@@ -1,24 +1,27 @@
 require 'knife-spork/plugins/git'
 require 'app_conf'
+require 'tempfile'
 
 module KnifeSpork::Plugins
   describe Git do
     it "checks if hook is allowed to auto_push" do
-      config = AppConf.new
-      config.from_hash({
-        "plugins" => {
-          "git" => {
-            "auto_push" => {
-              "disabled" => [
-                "after_envgroup_attribute_set"
-              ]
-            }
-          }
-        }
-      })
+      Tempfile.open('config.yml') do |f|
+        f.write(<<-EOF)
+        plugins:
+          git:
+            auto_push:
+              disabled:
+                - after_envgroup_attribute_set
+        EOF
 
-      git_plugin = Git.new(:config => config)
-      expect(git_plugin.auto_push_disabled?(:after_envgroup_attribute_set)).to eq true
+        f.close
+
+        config = AppConf.new
+        config.load(f.path)
+
+        git_plugin = Git.new(:config => config)
+        expect(git_plugin.auto_push_disabled?(:after_envgroup_attribute_set)).to eq true
+      end
     end
   end
 end
