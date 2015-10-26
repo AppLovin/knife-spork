500bead3168bd227800d881ad2ce5d944c247737
jeunito <jeunito@gmail.com>
2015-10-26 15:07:38 -0700

    do not raise error if -m is not passed

--- lib/knife-spork/plugins/git.rb
+++ lib/knife-spork/plugins/git.rb
@@ -302,10 +302,8 @@ module KnifeSpork
             git_add("#{databag_path}/#{object_name}", object_secondary_name)
             git_commit(databag_path, @options[:args][:git_message])
             git_push(branch)
-          else
-            raise "Git message (-m) required since git auto_push is enabled"
           end
-        end         
+        end
       end
 
       def before_nodefromfile
