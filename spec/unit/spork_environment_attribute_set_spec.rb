39fbb31d3f0957dd62fe517a99cc1dff6c4b3cd1
jeunito <jeunito@gmail.com>
2015-11-02 19:31:57 -0800

    Revert "accept hash"
    This reverts commit c3fae2891285ae956b70a4a8d46395f91492d1b0.

--- spec/unit/spork_environment_attribute_set_spec.rb
+++ spec/unit/spork_environment_attribute_set_spec.rb
@@ -19,7 +19,6 @@ module KnifeSpork
         allow(k).to receive(:save_environment_changes)
         allow(k).to receive(:load_environment_from_file).with("TestEnvironment1").and_return(test_environment1)
         allow(k).to receive(:load_environment_from_file).with("TestEnvironment2").and_return(test_environment2)
-        allow(k).to receive(:config).and_return({})
       end
     end
 
@@ -28,38 +27,23 @@ module KnifeSpork
     let(:test_environment1) do
       double().tap do |d|
         allow(d).to receive(:to_hash)
-        allow(d).to receive(:save)
       end
     end
 
     let(:test_environment2) do
       double().tap do |d|
         allow(d).to receive(:to_hash)
-        allow(d).to receive(:save)
       end
     end
 
     describe "#run" do
-      context "when --no_upload is passed" do
-        it "does not upload environment if --no_upload is passed" do
-          allow(knife).to receive(:config).and_return({ :create_if_missing => true , :no_upload => true })
+      it "does not upload environment if --no_upload is passed" do
+        allow(knife).to receive(:config).and_return({ :create_if_missing => true , :no_upload => true })
 
-          expect(test_environment1).not_to receive(:save)
-          expect(test_environment2).not_to receive(:save)
+        expect(test_environment1).not_to receive(:save)
+        expect(test_environment2).not_to receive(:save)
 
-          knife.run
-        end
-      end
-
-      context "when json is passed" do
-        let(:argv) { [ 'test', 'hello', '{ "value" : "world" }' ] }
-
-        it "passes hash to override_attribute" do
-          expect(knife).to receive(:override_attribute).with("hello", { "value" => "world" }, test_environment1, false, false )
-          expect(knife).to receive(:override_attribute).with("hello", { "value" => "world" }, test_environment2, false, false )
-
-          knife.run
-        end
+        knife.run
       end
     end
   end
