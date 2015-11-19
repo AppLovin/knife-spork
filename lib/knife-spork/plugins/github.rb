ac44bf754d16db65c073cdc6be8a6b77a8d286f8
jeunito <jeunito@gmail.com>
2015-11-18 19:16:58 -0800

    temporarily include git in github plugin

--- lib/knife-spork/plugins/github.rb
+++ lib/knife-spork/plugins/github.rb
@@ -30,6 +30,39 @@ module KnifeSpork
           github.create_pull_request(*pull_args)
         end
       end
+      
+      private
+      def git
+        safe_require 'git'
+        log = Logger.new(STDOUT)
+        log.level = Logger::WARN
+        @git ||= begin
+          cwd = FileUtils.pwd()
+          ::Git.open(get_parent_dir(cwd) , :log => log)
+        rescue Exception => e  
+          ui.error "You are not currently in a git repository #{cwd}. Please ensure you are in a git repo, a repo subdirectory, or remove the git plugin from your KnifeSpork configuration!"
+          exit(0)
+        end
+      end
+
+      def get_parent_dir(path)
+        top_level = path
+        return_code = 0
+        while return_code == 0
+          output = IO.popen("cd #{top_level}/.. && git rev-parse --show-toplevel 2>&1")
+          Process.wait
+          return_code = $?
+          cmd_output = output.read.chomp
+          #cygwin, I hate you for making me do this
+          if cmd_output.include?("fatal: Not a git repository")
+            return_code = 1
+          end
+          if return_code == 0
+            top_level = cmd_output
+          end
+        end
+        top_level
+      end
     end
   end
 end
